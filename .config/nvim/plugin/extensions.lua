944d474730a0bb7be834427c9fb061476c89da1a
Moritz Goeckel <moritz.goeckel@sap.com>
2023-09-05 12:18:59 +0000

    Call in new tab for vim

--- .config/nvim/plugin/extensions.lua
+++ .config/nvim/plugin/extensions.lua
@@ -1,20 +1,31 @@
+function callInNewTab(cmd)
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+
+    lines = {}
+    for s in result:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+
+    handle:close()
+    vim.api.nvim_command('tabnew')
+    vim.api.nvim_buf_set_lines(0, 0, 0, 0, lines)
+    vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
+    vim.api.nvim_buf_set_option(0, 'modifiable', false)
+end
+
 vim.api.nvim_create_user_command('FindF',
     function(opts)
         local cmd = 'find . -iname "*'..opts.fargs[1]..'*" -not -path "./build/*"'
-        print(cmd)
-        local handle = io.popen(cmd)
-        local result = handle:read("*a")
+        callInNewTab(cmd)
+    end,
+    { nargs = 1 })
 
-        lines = {}
-        for s in result:gmatch("[^\r\n]+") do
-            table.insert(lines, s)
-        end
 
-        handle:close()
-        vim.api.nvim_command('tabnew')
-        vim.api.nvim_buf_set_lines(0, 0, 0, 0, lines)
-        vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
-        vim.api.nvim_buf_set_option(0, 'modifiable', false)
+vim.api.nvim_create_user_command('FindT',
+    function(opts)
+        local cmd = 'git grep --no-color "'..opts.fargs[1]..'"'
+        callInNewTab(cmd)
     end,
     { nargs = 1 })
 
